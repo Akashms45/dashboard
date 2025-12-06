ec649f2da6f624fa5e2b4e4f01bd4cd03e3cd2bf
Akashms45 <akashms452002@gmail.com>
2025-12-06 18:47:31 +0530

    routers

--- src/pages/Analytics/index.js
+++ src/pages/Analytics/index.js
@@ -4,17 +4,17 @@ import { Navbar } from "../../components/navbar";
 export const Analytics = () => {
   return (
     <>
-      <div className="flex flex-col">
-        <div className="flex">
+      <div className="flex">
+        <div className="flex-none">
           <Sidebar />
         </div>
 
-        <div className="flex ">
+        <div className="flex-1 ">
           <Navbar />
-        </div>
-        <div className="flex">
-          <div className="h-12 sm:h-16 md:h-[816px] w-screen bg-neutral-100 shadow-inner flex   px-2 sm:px-4 md:px-6 ml-72 mr-5 rounded-xl ">
-            <h1 className="p-2 mt-4 text-4xl font-bold">Analytics</h1>
+          <div className="flex">
+            <div className="h-12 sm:h-16 md:h-[805px] w-full bg-neutral-100 shadow-inner  px-2 sm:px-4 md:px-6 ml-1  rounded-xl ">
+              <h1 className="p-2 mt-4 text-4xl font-bold">Analytics</h1>
+            </div>
           </div>
         </div>
       </div>
